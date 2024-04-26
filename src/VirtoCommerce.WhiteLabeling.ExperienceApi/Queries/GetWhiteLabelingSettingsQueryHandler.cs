a47561f84629f2dcc3ef388ab35e1a313a7911d7
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-04-26 12:08:54 +0200

    fix: remove mock data

--- src/VirtoCommerce.WhiteLabeling.ExperienceApi/Queries/GetWhiteLabelingSettingsQueryHandler.cs
+++ src/VirtoCommerce.WhiteLabeling.ExperienceApi/Queries/GetWhiteLabelingSettingsQueryHandler.cs
@@ -1,15 +1,12 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
-using VirtoCommerce.ContentModule.Core.Model;
 using VirtoCommerce.CustomerModule.Core.Model;
 using VirtoCommerce.CustomerModule.Core.Services;
 using VirtoCommerce.ExperienceApiModule.Core.Infrastructure;
-using VirtoCommerce.ExperienceApiModule.XCMS;
 using VirtoCommerce.ExperienceApiModule.XCMS.Queries;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.WhiteLabeling.Core.Models;
@@ -35,13 +32,6 @@ namespace VirtoCommerce.WhiteLabeling.ExperienceApi.Queries
 
         public async Task<ExpWhiteLabelingSetting> Handle(GetWhiteLabelingSettingsQuery request, CancellationToken cancellationToken)
         {
-            // temp
-            var mockResult = GetMockData(request);
-            if (mockResult != null)
-            {
-                return mockResult;
-            }
-
             var searchCriteria = AbstractTypeFactory<WhiteLabelingSettingSearchCriteria>.TryCreateInstance();
             searchCriteria.IsEnabled = true;
             searchCriteria.OrganizationId = request.OrganizationId;
@@ -110,206 +100,6 @@ namespace VirtoCommerce.WhiteLabeling.ExperienceApi.Queries
             var result = new Uri(new Uri(uriWithoutLastSegment), newName);
 
             return result.AbsoluteUri;
-        }
-
-        private static ExpWhiteLabelingSetting GetMockData(GetWhiteLabelingSettingsQuery request)
-        {
-            var mockData = new List<ExpWhiteLabelingSetting>()
-            {
-                new ExpWhiteLabelingSetting()
-                {
-                    LabelingSetting = new WhiteLabelingSetting()
-                    {
-                        UserId = "cfcec63a-faa5-4511-9aef-ee7672af6710",
-                        OrganizationId = "f081c52234754c9c8229aa42d6a19220",
-                        LogoUrl = "https://vcst-dev.govirto.com/cms-content/assets/b2b-store-assets/logo-white-labeling-test.svg",
-                        SecondaryLogoUrl = "https://vcst-dev.govirto.com/cms-content/assets/b2b-store-assets/logo-inverted-white-labeling-test.svg",
-                    },
-                    Favicons = new List<ExpFavicon>()
-                    {
-                        new ExpFavicon()
-                        {
-                            Rel = "shortcut icon",
-                            Href = "https://vcst-dev.govirto.com/cms-content/assets/b2b-store-assets/favicon.ico"
-                        },
-                        new ExpFavicon()
-                        {
-                            Rel = "icon",
-                            Type = "icon/png",
-                            Sizes = "16x16",
-                            Href = "https://vcst-dev.govirto.com/cms-content/assets/b2b-store-assets/favicon-16x16.png"
-                        },
-                        new ExpFavicon()
-                        {
-                            Rel = "icon",
-                            Type = "icon/png",
-                            Sizes = "32x32",
-                            Href = "https://vcst-dev.govirto.com/cms-content/assets/b2b-store-assets/favicon-32x32.png"
-                        },
-                        new ExpFavicon()
-                        {
-                            Type = "manifest",
-                            Href = "https://vcst-dev.govirto.com/cms-content/assets/b2b-store-assets/manifest.json"
-                        },
-                    },
-                    FooterLinks = new List<MenuItem>()
-                    {
-                        new MenuItem()
-                        {
-                            Link = new MenuLink()
-                            {
-                                Title = "Company Details",
-                                Url = "/company-details"
-                            },
-                            ChildItems = new List<MenuItem>()
-                            {
-                                new MenuItem()
-                                {
-                                    Link = new MenuLink()
-                                    {
-                                        Title = "About Us",
-                                        Url = "/about-us"
-                                    }
-                                },
-                                new MenuItem()
-                                {
-                                    Link = new MenuLink()
-                                    {
-                                        Title = "Investor Relations",
-                                        Url = "/investor-relations"
-                                    }
-                                }
-                            }
-                        },
-                        new MenuItem()
-                        {
-                            Link = new MenuLink()
-                            {
-                                Title = "Customer Support",
-                                Url = "/customer-support"
-                            },
-                            ChildItems = new List<MenuItem>()
-                            {
-                                new MenuItem()
-                                {
-                                    Link = new MenuLink()
-                                    {
-                                        Title = "Catalog Request",
-                                        Url = "/catalog-request"
-                                    }
-                                },
-                                new MenuItem()
-                                {
-                                    Link = new MenuLink()
-                                    {
-                                        Title = "Contact Us",
-                                        Url = "/contact-us"
-                                    }
-                                }
-                            }
-                        }
-
-                    },
-                },
-                // QA test entry
-                new ExpWhiteLabelingSetting()
-                {
-                    LabelingSetting = new WhiteLabelingSetting()
-                    {
-                        UserId = "de8ec5fa-4633-4d81-bb53-8e7ac23a057b",
-                        OrganizationId = "05763259-a5d3-4650-ad75-51f416a6368e",
-                        LogoUrl = "https://vcst-qa.govirto.com/cms-content/assets/b2b-store-assets/logo-white-labeling-test.svg",
-                        SecondaryLogoUrl = "https://vcst-qa.govirto.com/cms-content/assets/b2b-store-assets/logo-inverted-white-labeling-test.svg",
-                    },
-                    Favicons = new List<ExpFavicon>()
-                    {
-                        new ExpFavicon()
-                        {
-                            Rel = "shortcut icon",
-                            Href = "https://vcst-qa.govirto.com/cms-content/assets/b2b-store-assets/favicon.ico"
-                        },
-                        new ExpFavicon()
-                        {
-                            Rel = "icon",
-                            Type = "icon/png",
-                            Sizes = "16x16",
-                            Href = "https://vcst-qa.govirto.com/cms-content/assets/b2b-store-assets/favicon-16x16.png"
-                        },
-                        new ExpFavicon()
-                        {
-                            Rel = "icon",
-                            Type = "icon/png",
-                            Sizes = "32x32",
-                            Href = "https://vcst-qa.govirto.com/cms-content/assets/b2b-store-assets/favicon-32x32.png"
-                        },
-                        new ExpFavicon()
-                        {
-                            Type = "manifest",
-                            Href = "https://vcst-qa.govirto.com/cms-content/assets/b2b-store-assets/manifest.json"
-                        },
-                    },
-                    FooterLinks = new List<MenuItem>()
-                    {
-                        new MenuItem()
-                        {
-                            Link = new MenuLink()
-                            {
-                                Title = "Company Details",
-                                Url = "/company-details"
-                            },
-                            ChildItems = new List<MenuItem>()
-                            {
-                                new MenuItem()
-                                {
-                                    Link = new MenuLink()
-                                    {
-                                        Title = "About Us",
-                                        Url = "/about-us"
-                                    }
-                                },
-                                new MenuItem()
-                                {
-                                    Link = new MenuLink()
-                                    {
-                                        Title = "Investor Relations",
-                                        Url = "/investor-relations"
-                                    }
-                                }
-                            }
-                        },
-                        new MenuItem()
-                        {
-                            Link = new MenuLink()
-                            {
-                                Title = "Customer Support",
-                                Url = "/customer-support"
-                            },
-                            ChildItems = new List<MenuItem>()
-                            {
-                                new MenuItem()
-                                {
-                                    Link = new MenuLink()
-                                    {
-                                        Title = "Catalog Request",
-                                        Url = "/catalog-request"
-                                    }
-                                },
-                                new MenuItem()
-                                {
-                                    Link = new MenuLink()
-                                    {
-                                        Title = "Contact Us",
-                                        Url = "/contact-us"
-                                    }
-                                }
-                            }
-                        }
-                    }
-                },
-            };
-
-            var result = mockData.FirstOrDefault(x => x.LabelingSetting.OrganizationId == request.OrganizationId || x.LabelingSetting.UserId == request.UserId);
-            return result;
         }
     }
 }
