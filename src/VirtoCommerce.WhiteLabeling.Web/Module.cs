868465cafc7ad2062dc96d05cbff1ebe56208897
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-01-27 18:17:45 +0200

    fix: platform version

--- src/VirtoCommerce.WhiteLabeling.Web/Module.cs
+++ src/VirtoCommerce.WhiteLabeling.Web/Module.cs
@@ -1,7 +1,6 @@
 using System;
 using GraphQL;
 using GraphQL.MicrosoftDI;
-using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
