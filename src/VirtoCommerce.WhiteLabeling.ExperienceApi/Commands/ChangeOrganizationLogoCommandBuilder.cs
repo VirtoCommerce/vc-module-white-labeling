ea00d0ceb021b411108012d14358cecb15c8bf86
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-01-28 10:52:13 +0200

    fix: fix delete logo

--- src/VirtoCommerce.WhiteLabeling.ExperienceApi/Commands/ChangeOrganizationLogoCommandBuilder.cs
+++ src/VirtoCommerce.WhiteLabeling.ExperienceApi/Commands/ChangeOrganizationLogoCommandBuilder.cs
@@ -2,11 +2,14 @@ using System.Threading.Tasks;
 using GraphQL;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using VirtoCommerce.CustomerModule.Core.Model;
+using VirtoCommerce.FileExperienceApi.Core.Models;
 using VirtoCommerce.WhiteLabeling.ExperienceApi.Authorization;
 using VirtoCommerce.WhiteLabeling.ExperienceApi.Models;
 using VirtoCommerce.WhiteLabeling.ExperienceApi.Schemas;
 using VirtoCommerce.Xapi.Core.BaseQueries;
 using static VirtoCommerce.FileExperienceApi.Core.ModuleConstants.Security.Permissions;
+using static VirtoCommerce.WhiteLabeling.Core.ModuleConstants;
 
 namespace VirtoCommerce.WhiteLabeling.ExperienceApi.Commands;
 
@@ -24,6 +27,14 @@ public class ChangeOrganizationLogoCommandBuilder : CommandBuilder<ChangeOrganiz
     protected override async Task BeforeMediatorSend(IResolveFieldContext<object> context, ChangeOrganizationLogoCommand request)
     {
         await base.BeforeMediatorSend(context, request);
-        await Authorize(context, request.OrganizationId, new OrganizationLogoAuthorizationRequirement(Update));
+
+        var organizationFileResource = new File
+        {
+            OwnerEntityId = request.OrganizationId,
+            OwnerEntityType = nameof(Organization),
+            Scope = OrganizationLogoUploadScope,
+        };
+
+        await Authorize(context, organizationFileResource, new OrganizationLogoAuthorizationRequirement(Update));
     }
 }
